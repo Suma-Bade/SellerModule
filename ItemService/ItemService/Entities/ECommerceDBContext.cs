1ab87dc87c010aa93f596b90fedc07a6065c40ce
Suma-Bade <sumabade009@gmail.com>
2020-05-28 11:16:30 +0530

    28-05

---
+++ ItemService/ItemService/Entities/ECommerceDBContext.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ItemService.Entities
+{
+    public partial class ECommerceDBContext : DbContext
+    {
+        public ECommerceDBContext()
+        {
+        }
+
+        public ECommerceDBContext(DbContextOptions<ECommerceDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Items> Items { get; set; }
+        public virtual DbSet<Seller> Seller { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-S3RV3MH\\SQLEXPRESS;Initial Catalog=ECommerceDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Items>(entity =>
+            {
+                entity.HasKey(e => e.Itemid)
+                    .HasName("PK__Items__3213E83FA2233492");
+
+                entity.Property(e => e.Itemid)
+                    .HasColumnName("itemid")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Imagename)
+                    .HasColumnName("imagename")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Itemname)
+                    .IsRequired()
+                    .HasColumnName("itemname")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .IsRequired()
+                    .HasColumnName("price")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Remarks)
+                    .HasColumnName("remarks")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sellerid).HasColumnName("sellerid");
+
+                entity.Property(e => e.Stockno).HasColumnName("stockno");
+
+                entity.HasOne(d => d.Seller)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(d => d.Sellerid)
+                    .HasConstraintName("FK__Items__sellerid__21B6055D");
+            });
+
+            modelBuilder.Entity<Seller>(entity =>
+            {
+                entity.Property(e => e.Sellerid)
+                    .HasColumnName("sellerid")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Aboutcmpy)
+                    .HasColumnName("aboutcmpy")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasColumnName("address")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Companyname)
+                    .IsRequired()
+                    .HasColumnName("companyname")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gst).HasColumnName("gst");
+
+                entity.Property(e => e.Mobileno)
+                    .IsRequired()
+                    .HasColumnName("mobileno")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Website)
+                    .HasColumnName("website")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
